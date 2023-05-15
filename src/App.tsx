ef519e93ba5d60f68c6b0b2e8eb918de8ab339b2
Adarsh Inaganti <adarshinaganti@Adarsh-M1-Air.local>
2023-05-15 10:51:58 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import Like from "./components/Like";
+import {useState} from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 const App = () => {
+	const [selectedCategory, setSelectedCategory] = useState<string>("");
+
+	const [expenses, setExpenses] = useState([
+		{id: 1, description: "aaa", amount: 10, category: "Utilities"},
+		{id: 2, description: "bbb", amount: 20, category: "Utilities"},
+		{id: 3, description: "ccc", amount: 30, category: "Utilities"},
+		{id: 4, description: "ddd", amount: 40, category: "Utilities"},
+	]);
+
+	const visibleExpenses = selectedCategory
+		? expenses.filter((e) => e.category === selectedCategory)
+		: expenses;
+
 	return (
 		<div>
-			<Like onClick={() => console.log("clicked")} />
+			<div className="mb-5">
+				<ExpenseForm
+					onSubmit={(expense) =>
+						setExpenses([
+							...expenses,
+							{...expense, id: expenses.length + 1},
+						])
+					}
+				/>
+			</div>
+			<div className="mb-3">
+				<ExpenseFilter
+					onSelectCategory={(category) =>
+						setSelectedCategory(category)
+					}
+				/>
+			</div>
+			<ExpenseList
+				expenses={expenses}
+				onDelete={(id) =>
+					setExpenses(expenses.filter((e) => e.id !== id))
+				}
+			/>
 		</div>
 	);
 };
